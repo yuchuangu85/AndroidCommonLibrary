34b8e099f791d4b2670c7b91e5ac83bb038b6db7
yuchuangu85 <yuchuangu85@gmail.com>
2019-06-22 10:23:52 +0800

    添加新的库

---
+++ AndroidCleanArchitectureBoilerplate/presentation/src/main/java/org/buffer/android/boilerplate/presentation/model/BufferooView.kt
@@ -0,0 +1,6 @@
+package org.buffer.android.boilerplate.presentation.model
+
+/**
+ * Representation for a [BufferooView] instance for this layers Model representation
+ */
+class BufferooView(val name: String, val title: String, val avatar: String)
